cbd2663a2fbebf4822a73397031f47cb1b4a8415
nipeone <x0601y@live.com>
2020-03-09 16:47:26 +0800

    Update mask2coco.py
    从多线程处理更新胃多进程处理

--- mask2coco.py
+++ mask2coco.py
@@ -16,14 +16,17 @@ import fnmatch
 import datetime
 import cv2
 import numpy as np
+import random
+import collections
 from PIL import Image
 from pycococreatortools import pycococreatortools
 from pycocotools import mask
 
 from tqdm import tqdm
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
+import multiprocess
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+import itertools
 '''COCO 公共信息
 
 '''
@@ -104,67 +107,48 @@ def filter_for_annotations(root, files, image_filename):
     files = [f for f in files if re.match(file_name_prefix, os.path.splitext(os.path.basename(f))[0])]
     return files
 
-# def create_annotation_info(annotation_id, image_id, category_info, binary_mask, 
-#                            image_size=None, tolerance=2, bounding_box=None):
+class MultiProcessCounter():
+    ''' 多进程 计数器
+        param: ini 
+            计数器的初始值
+    '''
+    def __init__(self, ini=0):
+        self.val = multiprocess.Value('i', ini)
 
-#     if image_size is not None:
-#         binary_mask = pycococreatortools.resize_binary_mask(binary_mask, image_size)
+    def increment(self, delta=1):
+        with self.val.get_lock():
+            self.val.value += delta
+            return self.val.value
+    
+    @property
+    def value(self):
+        with self.val.get_lock():
+            return self.val.value
 
-#     binary_mask_encoded = mask.encode(np.asfortranarray(binary_mask.astype(np.uint8)))
-
-#     area = mask.area(binary_mask_encoded)
-#     #此处需要改造，针对camelyon normal类图片，
-#     if area < 1:
-#         return None
-
-#     if bounding_box is None:
-#         bounding_box = mask.toBbox(binary_mask_encoded)
-
-#     if category_info["is_crowd"]:
-#         is_crowd = 1
-#         segmentation = binary_mask_to_rle(binary_mask)
-#     else :
-#         is_crowd = 0
-#         segmentation = binary_mask_to_polygon(binary_mask, tolerance)
-#         if not segmentation:
-#             return None
-
-#     annotation_info = {
-#         "id": annotation_id,
-#         "image_id": image_id,
-#         "category_id": category_info["id"],
-#         "iscrowd": is_crowd,
-#         "area": area.tolist(),
-#         "bbox": bounding_box.tolist(),
-#         "segmentation": segmentation,
-#         "width": binary_mask.shape[1],
-#         "height": binary_mask.shape[0],
-#     } 
-
-#     return annotation_info
-
-def worker_create_annotation_info(image_filename, image_id)
+def worker_create_annotation_info(image_filename, image_id):
     image = Image.open(image_filename)
     image_info = pycococreatortools.create_image_info(image_id, os.path.basename(image_filename), image.size)
-
+    #多进程计数器必须标明global
+    global ann_counter
     # filter for associated png annotations
     annotation_infos=[]
     for root, _, files in os.walk(MASK_DIR):
         annotation_files = filter_for_annotations(root, files, image_filename)
         # go through each associated annotation
-        for segmentation_id, annotation_filename in enumerate(annotation_files):
+        for annotation_filename in annotation_files:
             class_id = [x['id'] for x in CATEGORIES if x['name'] in os.path.basename(annotation_filename)][0]                
             binary_mask = np.asarray(Image.open(annotation_filename).convert('1')).astype(np.uint8)
 
             category_info = {'id': class_id, 'is_crowd': 0}
             annotation_info = pycococreatortools.create_annotation_info(
-                    segmentation_id+1, image_id, category_info, binary_mask,
+                    ann_counter.increment(), image_id, category_info, binary_mask,
                     image.size, tolerance=2)
 
             if annotation_info is not None:
                 annotation_infos.append(annotation_info)
 
     return image_info, annotation_infos
+
 
 def parallel_gen_coco(num_workers):
     coco_output = {
@@ -174,11 +158,23 @@ def parallel_gen_coco(num_workers):
         "images": [],
         "annotations": []
     }
-    with ThreadPoolExecutor(num_workers) as pool:
+    #负样本比例太大，所以减小负样本数量
+    normal_image_infos={}
+    normal_annotation_infos={}
+    # ann_counter=itertools.count().__next__
+    
+    with ProcessPoolExecutor(num_workers) as pool:
         for root, _, files in os.walk(IMAGE_DIR):
             image_files = filter_for_image(root, files)
-            tasks = [pool.submit(worker_create_annotation_info,(image_filename, image_id+1) for image_id, image_filename in enumerate(image_files)]
-            for future in tqdm(as_completed(tasks), len(tasks)):
+            # 如果是肿瘤图片，直接添加。
+            # 如果是正常图片，先收拢一起，然后随机采样和肿瘤图片一样数量的，保正负样本均衡。
+            tumor_image_files = [image_filename for image_filename in image_files if 'tumor' in os.path.basename(image_filename)]
+            normal_image_files = random.sample([image_filename for image_filename in image_files if 'normal' in os.path.basename(image_filename)],len(tumor_image_files))
+
+            print(f"tumor nums:{len(tumor_image_files)},normal nums:{len(normal_image_files)}")
+
+            tasks = [pool.submit(worker_create_annotation_info, *(image_filename, image_id+1)) for image_id, image_filename in enumerate(tumor_image_files+normal_image_files)]
+            for future in tqdm(as_completed(tasks), total=len(tasks)):
                 try:
                     image_info, annotation_infos = future.result()
                     coco_output["images"].append(image_info)
@@ -189,14 +185,73 @@ def parallel_gen_coco(num_workers):
     with open(f'{ROOT_DIR}/instances_camelyon16_train2020.json', 'w') as output_json_file:
         json.dump(coco_output, output_json_file)
 
+
+def gen_coco():
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
+
+    image_id = 1
+    segmentation_id = 1
+
+    # filter for jpeg images
+    for root, _, files in os.walk(IMAGE_DIR):
+        image_files = filter_for_image(root, files)
+
+            # 如果是肿瘤图片，直接添加。
+            # 如果是正常图片，先收拢一起，然后随机采样和肿瘤图片一样数量的，保正负样本均衡。
+        tumor_image_files = [image_filename for image_filename in image_files if 'tumor' in os.path.basename(image_filename)]
+        normal_image_files = random.sample([image_filename for image_filename in image_files if 'normal' in os.path.basename(image_filename)],len(tumor_image_files))
+
+
+        # go through each image
+        for image_filename in tqdm(tumor_image_files+normal_image_files):
+            image = Image.open(image_filename)
+            image_info = pycococreatortools.create_image_info(
+                    image_id, os.path.basename(image_filename), image.size)
+            coco_output["images"].append(image_info)
+
+            # filter for associated png annotations
+            for root, _, files in os.walk(MASK_DIR):
+                annotation_files = filter_for_annotations(root, files, image_filename)
+
+                # go through each associated annotation
+                for annotation_filename in annotation_files:
+
+                    class_id = [x['id'] for x in CATEGORIES if x['name'] in os.path.basename(annotation_filename)][0]                
+                    binary_mask = np.asarray(Image.open(annotation_filename).convert('1')).astype(np.uint8)
+
+                    # class_id = 1 if binary_mask.sum() > 0 else 0
+                    category_info = {'id': class_id, 'is_crowd': 0}
+                    annotation_info = pycococreatortools.create_annotation_info(
+                            segmentation_id, image_id, category_info, binary_mask,
+                            image.size, tolerance=2)
+
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+
+                    segmentation_id = segmentation_id + 1        
+            image_id = image_id + 1
+
+    with open('{}/instances_camelyon16_train2020.json'.format(ROOT_DIR), 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+
+
 if __name__ == "__main__":
     # mask_dir = '/media/disk1/camelyon/train/masks'
     # label_list = glob.glob(os.path.join(mask_dir, '*.png'))
     # for label_name in label_list:
     #     rgb2binary(label_name)
 
+    # ROOT_DIR = '/media/disk1/camelyon/train'
+    ann_counter = MultiProcessCounter(0)
     ROOT_DIR = ''
     IMAGE_DIR = os.path.join(ROOT_DIR, "images")
     MASK_DIR = os.path.join(ROOT_DIR, "masks")
 
     parallel_gen_coco(8)
+    # gen_coco()
